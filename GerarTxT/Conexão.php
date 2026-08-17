efb45eae7331aa2e4d287cce3590b75bdcc6fcb2
gustavonemeth-hub <gustavo.nemeth@escola.pr.gov.br>
2026-08-17 20:18:56 -0300

    Add files via upload

---
+++ GerarTxT/Conexão.php
@@ -0,0 +1,14 @@
+<?php
+$usuario = "root";
+$senha = 'root';
+$dbname = "gerartxt";
+$host = 'localhost:3306';
+
+try
+{
+    $pdo = new PDO ("mysql:host=$host;dbname=$dbname", $usuario, $senha);
+}
+catch (PDOException $e)
+{
+    echo $e->getmessage();
+}
---
+++ GerarTxT/sucessobd.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+ 
+    <title>Sucesso Banco de Dados</title>
+</head>
+<body>
+
+    
+    <h2>Sucesso ao Gravar no Banco de Dados MySQLI</h2>
+
+    <a href="index.php" title="Ir para pagina inicial"> 
+        <img src="src/sucessobd.png" alt="index.php" width="680" height="509">
+    </a>
+
+</body>
+</html
